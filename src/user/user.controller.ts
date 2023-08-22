3382f86267f2a226f7700149304246c6199d5470
Luxing Li <luxingg.li@gmail.com>
2023-08-22 11:42:18 +1000

    nestjs api tutorial following

---
+++ src/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
+
+@Controller('users')
+@UseGuards(JwtGuard)
+export class UserController {
+    @Get('me')
+    getMe(@GetUser() user:User) {
+        return user;
+    }
+
+    @Patch()
+    editUser() {
+        
+    }
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,28 +1,91 @@
-import * as request from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
-import { INestApplication } from '@nestjs/common';
-
-describe('AppController (e2e)', () => {
+import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import * as pactum from 'pactum';
+import { AuthDto } from '../src/auth/dto';
+describe('App e2e', () => {
   let app: INestApplication;
-
+  let prisma: PrismaService;
   beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
+    const moduleRef = await Test.createTestingModule(
+      {
+        imports: [AppModule],
+      }).compile();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      })
+    );
     await app.init();
+
+    prisma = app.get(PrismaService)
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333')
   });
 
-  afterAll(async () => {
-    await app.close();
+  afterAll(() => {
+    app.close();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'ryan@atlanssian.com',
+      password: '123'
+    }
+    describe('Signup', () => {
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post(
+            '/auth/signup',
+          )
+          .withBody({
+            password: dto.password
+          })
+          .expectStatus(400)
+      });
+
+      it('should signup', () => {
+        return pactum
+          .spec()
+          .post(
+            '/auth/signup',
+          )
+          .withBody(dto)
+          .expectStatus(201)
+      });
+    });
+
+    describe('Signin', () => { 
+      it('should signin', () => {
+        return pactum
+          .spec()
+          .post(
+            '/auth/signin',
+          )
+          .withBody(dto)
+          .expectStatus(200)
+      });
+    });
   });
-});
+
+  describe('User', () => {
+    describe('Get me', () => { });
+
+    describe('Edit user', () => { });
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => { });
+
+    describe('Get bookmarks', () => { });
+
+    describe('Get bookmark by id', () => { });
+
+    describe('Edit bookmark', () => { });
+
+    describe('Delete bookmark', () => { });
+  });
+})
